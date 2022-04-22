afc14f3f92475f99016fd93eb86fe93211b5fb08
riyazahamad22 <riyaz@agaze.in>
2022-04-22 07:48:34 +0530

    Setting up styles

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        brightRed: "#ef233c",
+        brightRedLight: "#ff4d6d",
+        brightRedSubLight: "#e5383b",
+        darkBlue: "#023e7d",
+        darkGrayishBlue: "#023047",
+        veryDarkBlue: "#001233",
+        veryPalRed: "#f25c54",
+        veryLightGray: "#f25c54",
+      },
+    },
+  },
+  plugins: [],
+};
